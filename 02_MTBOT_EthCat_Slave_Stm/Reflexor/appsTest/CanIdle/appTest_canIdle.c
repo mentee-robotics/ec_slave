3ad28237e738cd3e53dc77ce79000ad080d0ff01
Minh Pham <minhpham.iot96@gmail.com>
2023-08-23 23:12:36 +0700

    Update missing files.

---
+++ 02_MTBOT_EthCat_Slave_Stm/Reflexor/appsTest/CanIdle/appTest_canIdle.c
@@ -0,0 +1,127 @@
+/**
+ * @file appTest_canIdle.c
+ *
+ * TODO: Comment
+ */
+
+/*---------------------------------------------------------------------------------------------------------------------
+ *                                                 INCLUDES
+ *-------------------------------------------------------------------------------------------------------------------*/
+
+#include <math.h>
+#include "md80.h"
+#include "canM_Types.h"
+#include "canM.h"
+#include "canIdle.h"
+#include "appTest_canIdle.h"
+#include "cmsis_os.h"
+#include "ethCat_types.h"
+
+/*---------------------------------------------------------------------------------------------------------------------
+ *                                                 DEFINES
+ *-------------------------------------------------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------------------------------------------------
+ *                                                VARIABLES
+ *-------------------------------------------------------------------------------------------------------------------*/
+
+static tAppTest_CanIdle appTest = {
+   .testCase = APPTEST_CANIDLE_NO,
+   .angleRad = 0.0f,
+   .dt = 0.01f,
+   .maxAngleRad = 2 * M_PI,
+   .rotationCycle = true,
+   .isCanIdleEnabled = false,
+   .md80id = 0,
+};
+
+/*---------------------------------------------------------------------------------------------------------------------
+ *                                            FUNCTION DEFINATIONS
+ *-------------------------------------------------------------------------------------------------------------------*/
+
+void AppTest_CanIdle_Init ()
+{
+
+}
+
+void AppTest_CanIdle_MainFunction ()
+{
+   uint8_t cmd = 0;
+   uint8_t length = 0;
+   uint8_t dataCmd[ETHCAT_LENGTH_DATAMAX] = {0};
+   
+   switch (appTest.testCase)
+   {
+   case APPTEST_CANIDLE_NO:
+
+      break;
+
+   case APPTEST_CANIDLE_BLINK:
+      md80_ConfigBlink (&appTest.dev);
+      break;
+
+   case APPTEST_CANIDLE_SCAN:
+
+      break;
+   
+   case APPTEST_CANIDLE_EXAMPLE8:
+      if (false == appTest.isCanIdleEnabled)
+      {
+    	   canM_SetAddrTxRxDev(DEV_CAN_0, appTest.md80id, appTest.md80id);
+
+      	osDelay(1000);
+
+         /* Reset encoder at current position */
+         md80_ControlMd80Mode (&appTest.dev, MD80_IMPEDANCE);
+         osDelay(1000);
+
+         md80_ControlMd80Enable (&appTest.dev, true);
+         osDelay(1000);
+
+         md80_SetImpedanceControllerParams(&appTest.dev, 10.0f, 1.0f);
+         osDelay(1000);
+
+         md80_SetEncoderZero(&appTest.dev);
+         osDelay(1000);
+
+         md80_SetMaxTorque (&appTest.dev, 10.0);
+         md80_SetMaxVelocity (&appTest.dev, 5.0);
+         osDelay(1000);
+
+         /* Set mode to position PID */
+
+         md80_SetCurrentLimit(&appTest.dev, 1.0f);
+         osDelay(500);
+
+         appTest.isCanIdleEnabled = true;
+      }
+
+      md80_SetTargetPosition (&appTest.dev , appTest.angleRad);
+
+      if (appTest.rotationCycle)
+      {
+         appTest.angleRad += appTest.dt;
+      }
+      else
+      {
+         appTest.angleRad -= appTest.dt;
+      }
+
+      if (appTest.angleRad >= appTest.maxAngleRad)
+      {
+    	   appTest.rotationCycle = false;
+      }
+      else if (appTest.angleRad <= 0)
+      {
+    	   appTest.rotationCycle = true;
+      }
+
+      md80_MainFunction (&appTest.dev);
+      osDelay(10);
+
+      break;
+   
+   default:
+      break;
+   }
+}
---
+++ 02_MTBOT_EthCat_Slave_Stm/Reflexor/appsTest/CanIdle/appTest_canIdle.h
@@ -0,0 +1,72 @@
+/**
+ * @file appTest_canIdle.h
+ *
+ * TODO: Comment.
+ */
+#ifndef APPTEST_CANIDLE_H_
+#define APPTEST_CANIDLE_H_
+
+/*---------------------------------------------------------------------------------------------------------------------
+ *                                                 INCLUDES
+ *-------------------------------------------------------------------------------------------------------------------*/
+
+#include <stdint.h>
+#include "md80.h"
+#include "canM_Types.h"
+
+/*---------------------------------------------------------------------------------------------------------------------
+ *                                                 DEFINES
+ *-------------------------------------------------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------------------------------------------------
+ *                                             GLOBAL VARIABLES
+ *-------------------------------------------------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------------------------------------------------
+ *                                               TYPES DEFINE
+ *-------------------------------------------------------------------------------------------------------------------*/
+
+typedef enum 
+{
+   APPTEST_CANIDLE_NO = 0,
+   APPTEST_CANIDLE_BLINK,
+   APPTEST_CANIDLE_SCAN,
+   APPTEST_CANIDLE_ADD_MD80,
+   APPTEST_CANIDLE_CONFIGURE_BAUDRATE,
+   APPTEST_CANIDLE_MD80_SET_BAUDRATE,
+   APPTEST_CANIDLE_EXAMPLE1,
+   APPTEST_CANIDLE_EXAMPLE2,
+   APPTEST_CANIDLE_EXAMPLE3,
+   APPTEST_CANIDLE_EXAMPLE4,
+   APPTEST_CANIDLE_EXAMPLE5,
+   APPTEST_CANIDLE_EXAMPLE6,
+   APPTEST_CANIDLE_EXAMPLE7,
+   APPTEST_CANIDLE_EXAMPLE8,
+   APPTEST_CANIDLE_EXAMPLE9,
+} tAppTest_CanIdle_Case;
+
+/* The data structure contains used for this test. */
+typedef struct 
+{
+   uint16_t md80id;
+   tMd80_Device dev;
+   tAppTest_CanIdle_Case testCase;
+   float angleRad;
+   float dt;
+   float maxAngleRad;
+   bool rotationCycle;
+   bool isCanIdleEnabled;
+} tAppTest_CanIdle;
+
+/*---------------------------------------------------------------------------------------------------------------------
+ *                                           VARIABLES DECLARATION
+ *-------------------------------------------------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------------------------------------------------
+ *                                            FUNCTION DECLARATION
+ *-------------------------------------------------------------------------------------------------------------------*/
+
+void AppTest_CanIdle_Init ();
+void AppTest_CanIdle_MainFunction ();
+
+#endif /* APPTEST_CANIDLE_H_ */
